0ccb5263f3aa61b6a48facd3ccb87d6c77600687
Tony Wu <theambiguousninja@gmail.com>
2021-01-23 18:17:10 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+svar words1 = ["stretch", "synchronize swim", "do the splits", "become enlightened", "soul search", "yodel"]
+var words2 = ["watching tv", "hiking", "committing felony fraud", "scamming kids", "eating dinner", "watching Boku no Pico", "sliding into your DMs"]
+
+function generate_alvinism() {
+    document.getElementById("verb1").innerHTML = words1[Math.floor(Math.random()*words1.length)];
+    document.getElementById("verb2").innerHTML = words2[Math.floor(Math.random()*words2.length)];
+}
