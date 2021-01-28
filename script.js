4f62b1e0af9d6ceb0ac9a48852ec52712b761b1a
Tony Wu <theambiguousninja@gmail.com>
2021-01-28 06:34:56 -0800

    more words

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var words1 = ["do yoga", "meditate", "pray", "cook", "stretch", "synchronize swim", "do the splits", "become enlightened", "soul search", "yodel"]
-var words2 = ["watching tv", "hiking", "committing felony fraud", "scamming kids", "eating dinner", "watching Boku no Pico", "sliding into your DMs", "showering", "doing anagrams", "building a PC"]
+var words1 = ["do insurrection", "trade GME", "do yoga", "meditate", "pray", "cook", "stretch", "synchronize swim", "do the splits", "become enlightened", "soul search", "yodel"]
+var words2 = ["browsing /r/anime", "watching tv", "hiking", "committing felony fraud", "scamming kids", "eating dinner", "watching Boku no Pico", "sliding into your DMs", "showering", "doing anagrams", "building a PC"]
 
 function generate_alvinism() {
     document.getElementById("verb1").innerHTML = words1[Math.floor(Math.random()*words1.length)];
