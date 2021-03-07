8318df18747ba9c9a8291d58017d60c3a9fc05fb
Tony Wu <theambiguousninja@gmail.com>
2021-03-06 17:52:23 -0800

    move words list to file

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-var words1 = ["do insurrection", "trade GME", "do yoga", "meditate", "pray", "cook", "stretch", "synchronize swim", "do the splits", "become enlightened", "soul search", "yodel"]
-var words2 = ["browsing /r/anime", "watching tv", "hiking", "committing felony fraud", "scamming kids", "eating dinner", "watching Boku no Pico", "sliding into your DMs", "showering", "doing anagrams", "building a PC"]
-
-
+// load straight facts
 var words1 = []
+var words2 = []
+fetch("words1.txt").then( r => r.text() ).then( text => words1 = text.split('\n'))
+fetch("words2.txt").then( r => r.text() ).then( text => words2 = text.split('\n'))
 
 function generate_alvinism() {
-    const fileUrl = "words1.txt"
-    fetch(fileUrl).then( r => r.text() ).then( text => console.log(text) )
-
     document.getElementById("verb1").innerHTML = words1[Math.floor(Math.random()*words1.length)];
     document.getElementById("verb2").innerHTML = words2[Math.floor(Math.random()*words2.length)];
 }
